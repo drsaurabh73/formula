a5cfaca2db10f1579819b84df55bbffb54f85296
drsaurabh73 <drsaurabh73@gmail.com>
2022-03-09 05:48:20 +0000

    formula

---
+++ src/bidmas/add1.java
@@ -0,0 +1,13 @@
+package bidmas;
+
+public class add1 {
+    public static void main (String [] args ){
+        int A = 5;
+        int B= 10;
+        int C= 12;
+        int D= 22;
+        System.out.println((A+B)*C);
+        System.out.println((C-B)*A);
+        System.out.println((D-A)%C);
+    }
+}
